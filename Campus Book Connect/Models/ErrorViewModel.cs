696351c88eef20672a84b800393fb195b2d7885a
tregi <tregi@TRE>
2025-07-09 14:27:00 -0500

    Add project files.

---
+++ Campus Book Connect/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Campus_Book_Connect.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
