c1f817a9f64ef3a1a8f1c0c909113995952e47af
Richard Gidrey <tregidrey@gmail.com>
2025-07-18 17:26:50 -0500

    set up layout and navigation also added marketplace, trade ,login,and register pages ("feel free to change how it looks")

---
+++ Campus Book Connect/Controllers/MarketplaceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Campus_Book_Connect.Controllers
+{
+    public class MarketplaceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Campus Book Connect/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Campus_Book_Connect.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
